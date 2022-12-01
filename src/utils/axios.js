d950d1f28600bf65d1724ed1e985f96b6274742d
kathyweiii <katherine871209@gmail.com>
2022-12-01 13:57:47 +0800

    api change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,8 @@ export const languageRequest = axios.create({
   baseURL: "https://translate-plus.p.rapidapi.com/",
   headers: {
     // "X-RapidAPI-Key": "70a4432f68msha24cef369d8a137p1989c8jsne705f69c5e6c",
-    "X-RapidAPI-Key": "e55a0145afmsh63d2f7d5dc28088p14e733jsne71c16274560",
+    // "X-RapidAPI-Key": "e55a0145afmsh63d2f7d5dc28088p14e733jsne71c16274560",
+    "X-RapidAPI-Key": "701d4e5029mshe84c89feb202844p10a147jsna98c75e4fa35",
     "X-RapidAPI-Host": "translate-plus.p.rapidapi.com",
   },
 });
